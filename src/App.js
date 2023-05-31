cbdd724ec5adb4aa8356acd7d9bf663aea6ba2cd
ironlionm4n <ironlionm4n@gmail.com>
2023-05-30 20:54:37 -0400

    starting landing page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@ import logo from './logo.svg'
 import './App.css'
 import { Box, Typography } from '@mui/material'
 import headshot from './images/headshot.png'
+import { baseBlue, highlightBlue, highlightWhite, beige } from './colors'
+
 
 function App () {
+
+
   return (
-    <Box>
-      <img src={headshot} alt='Faceshot' />
-      <Typography>Mikail Miller</Typography>
-      <Typography>Hello World</Typography>
+    <Box bgcolor={baseBlue} height='100vh' width='100vw'>
+      <Box >
+        <Box
+          display='flex'
+          flexDirection='row'
+          justifyContent='space-evenly'
+          p={3}
+          ml={3}
+          mr={3}
+          gap='1.5rem'
+          bgcolor={highlightBlue}
+        >
+          <img
+            src={headshot}
+            alt='Faceshot'
+            style={{ height: '15rem', width: '15rem', borderRadius: '270px' }}
+          />
+          <Box
+            display='flex'
+            justifyContent='space-evenly'
+            alignItems='center'
+            width='100%'
+            p={4}
+          >
+            <Typography variant='h1' fontSize='48px'>
+              Mikail Miller
+            </Typography>
+            <Typography variant='h1' fontSize='32px'>
+              React & Unity Developer
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
---
+++ src/colors/index.js
@@ -0,0 +1,4 @@
+export const baseBlue = '#0096C8'
+export const highlightBlue = '#00BFFF'
+export const highlightWhite = '#E6F4F1'
+export const beige = '#EEE8A9'
